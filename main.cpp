2ab2bae89d8598b8c71afeefee0db5c9a8449461
Luca Mazzotta <lucamazzotta711@gmail.com>
2023-03-17 15:24:08 +0000

    Added TimeIntegrate and output

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 #include <tgmath.h>
 #include <boost/program_options.hpp>
 
@@ -157,10 +159,178 @@ public:
             }
         }
     }
-    void TimeIntegrate()
+    void TimeIntegrate(char type)
     {
-        return;
+        // Initialising variables
+        double *u = new double[Nx * Ny];
+        double *v = new double[Nx * Ny];
+        double *h = new double[Nx * Ny];
+        double *hu = new double[Nx * Ny];
+        double *hv = new double[Nx * Ny];
+
+        double *dudx = new double[Nx * Ny];
+        double *dudy = new double[Nx * Ny];
+
+        double *dvdx = new double[Nx * Ny];
+        double *dvdy = new double[Nx * Ny];
+
+        double *dhdx = new double[Nx * Ny];
+        double *dhdy = new double[Nx * Ny];
+
+        double *dhudx = new double[Nx * Ny];
+        double *dhvdy = new double[Nx * Ny];
+
+        double *k1u = new double[Nx * Ny];
+        double *k1v = new double[Nx * Ny];
+        double *k1h = new double[Nx * Ny];
+
+        double *k2u = new double[Nx * Ny];
+        double *k2v = new double[Nx * Ny];
+        double *k2h = new double[Nx * Ny];
+
+        double *k3u = new double[Nx * Ny];
+        double *k3v = new double[Nx * Ny];
+        double *k3h = new double[Nx * Ny];
+
+        double *k4u = new double[Nx * Ny];
+        double *k4v = new double[Nx * Ny];
+        double *k4h = new double[Nx * Ny];
+
+        double g = 9.81;
+
+        for (int j = 0; j < T / dt; ++j)
+        {
+            // Copying U, V, H to u, v, h (temp arrays) and calculating hu, hv
+            for (int i = 0; i < Nx * Ny; ++i)
+            {
+                u[i] = U[i];
+                v[i] = V[i];
+                h[i] = H[i];
+                hu[i] = H[i] * U[i];
+                hv[i] = H[i] * V[i];
+            }
+
+            // Calculating K1
+            xDerivative(type, u, dudx);
+            yDerivative(type, u, dudy);
+            xDerivative(type, v, dvdx);
+            yDerivative(type, v, dvdy);
+            xDerivative(type, h, dhdx);
+            yDerivative(type, h, dhdy);
+            xDerivative(type, hu, dhudx);
+            yDerivative(type, hv, dhvdy);
+
+            for (int i = 0; i < Nx * Ny; ++i)
+            {
+                k1u[i] = -u[i] * dudx[i] - v[i] * dudy[i] - g * dhdx[i];
+                k1v[i] = -u[i] * dvdx[i] - v[i] * dvdy[i] - g * dhdy[i];
+                k1h[i] = -dhudx[i] - dhvdy[i];
+            }
+
+            // Calculating K2
+            for (int i = 0; i < Nx * Ny; ++i)
+            {
+                u[i] = U[i] + k1u[i] * dt / 2;
+                v[i] = V[i] + k1v[i] * dt / 2;
+                h[i] = H[i] + k1h[i] * dt / 2;
+                hu[i] = h[i] * u[i];
+                hv[i] = h[i] * v[i];
+            }
+
+            xDerivative(type, u, dudx);
+            yDerivative(type, u, dudy);
+            xDerivative(type, v, dvdx);
+            yDerivative(type, v, dvdy);
+            xDerivative(type, h, dhdx);
+            yDerivative(type, h, dhdy);
+            xDerivative(type, hu, dhudx);
+            yDerivative(type, hv, dhvdy);
+
+            for (int i = 0; i < Nx * Ny; ++i)
+            {
+                k2u[i] = -u[i] * dudx[i] - v[i] * dudy[i] - g * dhdx[i];
+                k2v[i] = -u[i] * dvdx[i] - v[i] * dvdy[i] - g * dhdy[i];
+                k2h[i] = -dhudx[i] - dhvdy[i];
+            }
+
+            // Calculating K3
+            for (int i = 0; i < Nx * Ny; ++i)
+            {
+                u[i] = U[i] + k2u[i] * dt / 2;
+                v[i] = V[i] + k2v[i] * dt / 2;
+                h[i] = H[i] + k2h[i] * dt / 2;
+                hu[i] = h[i] * u[i];
+                hv[i] = h[i] * v[i];
+            }
+
+            xDerivative(type, u, dudx);
+            yDerivative(type, u, dudy);
+            xDerivative(type, v, dvdx);
+            yDerivative(type, v, dvdy);
+            xDerivative(type, h, dhdx);
+            yDerivative(type, h, dhdy);
+            xDerivative(type, hu, dhudx);
+            yDerivative(type, hv, dhvdy);
+
+            for (int i = 0; i < Nx * Ny; ++i)
+            {
+                k3u[i] = -u[i] * dudx[i] - v[i] * dudy[i] - g * dhdx[i];
+                k3v[i] = -u[i] * dvdx[i] - v[i] * dvdy[i] - g * dhdy[i];
+                k3h[i] = -dhudx[i] - dhvdy[i];
+            }
+
+            // Calculating K4
+            for (int i = 0; i < Nx * Ny; ++i)
+            {
+                u[i] = U[i] + k3u[i] * dt;
+                v[i] = V[i] + k3v[i] * dt;
+                h[i] = H[i] + k3h[i] * dt;
+                hu[i] = h[i] * u[i];
+                hv[i] = h[i] * v[i];
+            }
+
+            xDerivative(type, u, dudx);
+            yDerivative(type, u, dudy);
+            xDerivative(type, v, dvdx);
+            yDerivative(type, v, dvdy);
+            xDerivative(type, h, dhdx);
+            yDerivative(type, h, dhdy);
+            xDerivative(type, hu, dhudx);
+            yDerivative(type, hv, dhvdy);
+
+            for (int i = 0; i < Nx * Ny; ++i)
+            {
+                k4u[i] = -u[i] * dudx[i] - v[i] * dudy[i] - g * dhdx[i];
+                k4v[i] = -u[i] * dvdx[i] - v[i] * dvdy[i] - g * dhdy[i];
+                k4h[i] = -dhudx[i] - dhvdy[i];
+            }
+
+            // Calculating U, V, and H
+            for (int i = 0; i < Nx * Ny; ++i)
+            {
+                U[i] = U[i] + (k1u[i] + 2 * k2u[i] + 2 * k3u[i] + k4u[i]) * dt / 6;
+                V[i] = V[i] + (k1v[i] + 2 * k2v[i] + 2 * k3v[i] + k4v[i]) * dt / 6;
+                H[i] = H[i] + (k1h[i] + 2 * k2h[i] + 2 * k3h[i] + k4h[i]) * dt / 6;
+            }
+        }
     }
+
+    void output() 
+    {
+        ofstream out("output.txt", ios::out | ios::trunc);
+
+        for (int i = 0; i<Ny; ++i) 
+        {
+            for (int j = 0; j<Nx; ++j) 
+            {
+                out << j << ' ' << i << ' ' << U[i + Ny * j] << ' ' << V[i + Ny * j] << ' ' << H[i + Ny * j] << endl;
+            }
+            out << endl;
+        }
+
+        out.close();
+    }
+
     void print()
     {
         cout << "dt = " << dt << endl;
@@ -208,5 +378,9 @@ int main(int argc, char **argv)
 
     obj.SetInitialConditions();
 
+    obj.TimeIntegrate('L');
+
     obj.printmatrix(obj.getH());
+
+    obj.output();
 }
