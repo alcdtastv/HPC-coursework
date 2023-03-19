29487a5a05b22974318666d82f6b21090bf643aa
Luca Mazzotta <lucamazzotta711@gmail.com>
2023-03-19 02:40:49 +0000

    Implemented BLAS central difference scheme

--- main.cpp
+++ main.cpp
@@ -4,7 +4,18 @@
 #include <tgmath.h>
 #include <boost/program_options.hpp>
 
+#define F77NAME(x) x##_
+
 using namespace std;
+
+extern "C"
+{
+    void F77NAME(dgbmv)(const char &trans, const int &m, const int &n, const int &KL, const int &KU, const double &alpha,
+                        const double *A, const int &lda, const double *x, const int &incx, const double &beta, double *y,
+                        const int &incy);
+
+    double F77NAME(ddot)(const int &n, const double *x, const int &incx, const double *y, const int &incy);
+}
 
 class ShallowWater
 {
@@ -14,7 +25,7 @@ public:
         namespace po = boost::program_options;
 
         po::options_description desc("Allowed options");
-        desc.add_options()("dt", po::value<double>(&dt), "Time-step to use.")("T", po::value<double>(&T), "Total integration time.")("Nx", po::value<int>(&Nx), "Number of grid points in x")("Ny", po::value<int>(&Ny), "Number of grid points in y")("ic", po::value<int>(&ic), "Index of the initial condition to use (1-4)");
+        desc.add_options()("dt", po::value<double>(&dt), "Time-step to use.")("T", po::value<double>(&T), "Total integration time.")("Nx", po::value<int>(&Nx), "Number of grid points in x")("Ny", po::value<int>(&Ny), "Number of grid points in y")("ic", po::value<int>(&ic), "Index of the initial condition to use (1-4)")("type", po::value<char>(&type), "Blas or Loop");
 
         po::variables_map vm;
         po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -23,6 +34,38 @@ public:
         U = new double[Nx * Ny];
         V = new double[Nx * Ny];
         H = new double[Nx * Ny];
+
+        if (type == 'B')
+        {
+            stencil = new double[7 * Nx]; // defined as x stencil, transpose if calculating y derivative
+
+            for (int i = 0; i < Nx; ++i)
+            {
+                stencil[7 * i] = 1.0 / 60;
+                stencil[7 * i + 1] = -3.0 / 20;
+                stencil[7 * i + 2] = 3.0 / 4;
+                stencil[7 * i + 3] = 0;
+                stencil[7 * i + 4] = -3.0 / 4;
+                stencil[7 * i + 5] = 3.0 / 20;
+                stencil[7 * i + 6] = -1.0 / 60;
+            }
+
+            extrarowstencil = new double[Nx + 5];
+            extrarowstencil[0] = 3.0 / 4;
+            extrarowstencil[1] = -3.0 / 20;
+            extrarowstencil[2] = 1.0 / 60;
+            extrarowstencil[Nx + 2] = -1.0 / 60;
+            extrarowstencil[Nx + 3] = 3.0 / 20;
+            extrarowstencil[Nx + 4] = -3.0 / 4;
+
+            extracolumnstencil = new double[Ny + 5];
+            extracolumnstencil[0] = 3.0 / 4;
+            extracolumnstencil[1] = -3.0 / 20;
+            extracolumnstencil[2] = 1.0 / 60;
+            extracolumnstencil[Ny + 2] = -1.0 / 60;
+            extracolumnstencil[Ny + 3] = 3.0 / 20;
+            extracolumnstencil[Ny + 4] = -3.0 / 4;
+        }
     }
     void SetInitialConditions()
     {
@@ -71,95 +114,101 @@ public:
             cout << "Invalid initial condition" << endl;
         }
     }
-    void xDerivative(char type, double *in, double *out)
+    void xDerivative(double *in, double *out)
     {
         if (type == 'B')
         {
-            BlasDerivativeX(in, out);
+            for (int i = 0; i < Ny; ++i)
+            {
+                F77NAME(dgbmv)
+                ('N', Nx, Nx, 3, 3, 1.0, stencil, 7, in + i, Nx, 0, out + i, Nx);
+
+                out[i] += F77NAME(ddot)(Nx, extrarowstencil + 5, 1, in + i, Nx);
+                out[i + Ny] += F77NAME(ddot)(Nx, extrarowstencil + 4, 1, in + i, Nx);
+                out[i + 2 * Ny] += F77NAME(ddot)(Nx, extrarowstencil + 3, 1, in + i, Nx);
+                out[i + Nx * (Ny - 3)] += F77NAME(ddot)(Nx, extrarowstencil + 2, 1, in + i, Nx);
+                out[i + Nx * (Ny - 2)] += F77NAME(ddot)(Nx, extrarowstencil + 1, 1, in + i, Nx);
+                out[i + Nx * (Ny - 1)] += F77NAME(ddot)(Nx, extrarowstencil, 1, in + i, Nx);
+            }
         }
         else if (type == 'L')
         {
-            LoopDerivativeX(in, out);
+            for (int row = 0; row < Ny; ++row)
+            {
+                out[row] = -in[(Nx - 3) * Ny + row] / 60 + 3 * in[(Nx - 2) * Ny + row] / 20 - 3 * in[(Nx - 1) * Ny + row] / 4 +
+                           3 * in[Ny + row] / 4 - 3 * in[2 * Ny + row] / 20 + in[3 * Ny + row] / 60;
+
+                out[Ny + row] = -in[(Nx - 2) * Ny + row] / 60 + 3 * in[(Nx - 1) * Ny + row] / 20 - 3 * in[row] / 4 +
+                                3 * in[2 * Ny + row] / 4 - 3 * in[3 * Ny + row] / 20 + in[4 * Ny + row] / 60;
+
+                out[2 * Ny + row] = -in[(Nx - 1) * Ny + row] / 60 + 3 * in[row] / 20 - 3 * in[Ny + row] / 4 +
+                                    3 * in[3 * Ny + row] / 4 - 3 * in[4 * Ny + row] / 20 + in[5 * Ny + row] / 60;
+
+                out[(Nx - 3) * Ny + row] = -in[(Nx - 6) * Ny + row] / 60 + 3 * in[(Nx - 5) * Ny + row] / 20 - 3 * in[(Nx - 4) * Ny + row] / 4 +
+                                           3 * in[(Nx - 2) * Ny + row] / 4 - 3 * in[(Nx - 1) * Ny + row] / 20 + in[row] / 60;
+
+                out[(Nx - 2) * Ny + row] = -in[(Nx - 5) * Ny + row] / 60 + 3 * in[(Nx - 4) * Ny + row] / 20 - 3 * in[(Nx - 3) * Ny + row] / 4 +
+                                           3 * in[(Nx - 1) * Ny + row] / 4 - 3 * in[row] / 20 + in[Ny + row] / 60;
+
+                out[(Nx - 1) * Ny + row] = -in[(Nx - 4) * Ny + row] / 60 + 3 * in[(Nx - 3) * Ny + row] / 20 - 3 * in[(Nx - 2) * Ny + row] / 4 +
+                                           3 * in[row] / 4 - 3 * in[Ny + row] / 20 + in[2 * Ny + row] / 60;
+
+                for (int col = 3; col < Nx - 3; ++col)
+                {
+                    out[row + Ny * col] = -in[row + Ny * (col - 3)] / 60 + 3 * in[row + Ny * (col - 2)] / 20 - 3 * in[row + Ny * (col - 1)] / 4 +
+                                          3 * in[row + Ny * (col + 1)] / 4 - 3 * in[row + Ny * (col + 2)] / 20 + in[row + Ny * (col + 3)] / 60;
+                }
+            }
         }
     }
-    void yDerivative(char type, double *in, double *out)
+    void yDerivative(double *in, double *out)
     {
         if (type == 'B')
         {
-            BlasDerivativeY(in, out);
+            for (int i = 0; i < Ny; ++i)
+            {
+                F77NAME(dgbmv)
+                ('T', Ny, Ny, 3, 3, -1.0, stencil, 7, in + i * Ny, 1, 0, out + i * Ny, 1);
+
+                out[Ny * i] += F77NAME(ddot)(Ny, extracolumnstencil + 5, 1, in + i * Ny, 1);
+                out[Ny * i + 1] += F77NAME(ddot)(Ny, extracolumnstencil + 4, 1, in + i * Ny, 1);
+                out[Ny * i + 2] += F77NAME(ddot)(Ny, extracolumnstencil + 3, 1, in + i * Ny, 1);
+                out[Ny * (i + 1) - 3] += F77NAME(ddot)(Ny, extracolumnstencil + 2, 1, in + i * Ny, 1);
+                out[Ny * (i + 1) - 2] += F77NAME(ddot)(Ny, extracolumnstencil + 1, 1, in + i * Ny, 1);
+                out[Ny * (i + 1) - 1] += F77NAME(ddot)(Ny, extracolumnstencil, 1, in + i * Ny, 1);
+            }
         }
         else if (type == 'L')
         {
-            LoopDerivativeY(in, out);
-        }
-    }
-    void BlasDerivativeX(double *in, double *out)
-    {
-        return;
-    }
-    void BlasDerivativeY(double *in, double *out)
-    {
-        return;
-    }
-    void LoopDerivativeX(double *in, double *out)
-    {
-        for (int row = 0; row < Ny; ++row)
-        {
-            out[row] = -in[(Nx - 3) * Ny + row] / 60 + 3 * in[(Nx - 2) * Ny + row] / 20 - 3 * in[(Nx - 1) * Ny + row] / 4 +
-                       3 * in[Ny + row] / 4 - 3 * in[2 * Ny + row] / 20 + in[3 * Ny + row] / 60;
-
-            out[Ny + row] = -in[(Nx - 2) * Ny + row] / 60 + 3 * in[(Nx - 1) * Ny + row] / 20 - 3 * in[row] / 4 +
-                            3 * in[2 * Ny + row] / 4 - 3 * in[3 * Ny + row] / 20 + in[4 * Ny + row] / 60;
-
-            out[2 * Ny + row] = -in[(Nx - 1) * Ny + row] / 60 + 3 * in[row] / 20 - 3 * in[Ny + row] / 4 +
-                                3 * in[3 * Ny + row] / 4 - 3 * in[4 * Ny + row] / 20 + in[5 * Ny + row] / 60;
-
-            out[(Nx - 3) * Ny + row] = -in[(Nx - 6) * Ny + row] / 60 + 3 * in[(Nx - 5) * Ny + row] / 20 - 3 * in[(Nx - 4) * Ny + row] / 4 +
-                                       3 * in[(Nx - 2) * Ny + row] / 4 - 3 * in[(Nx - 1) * Ny + row] / 20 + in[row] / 60;
-
-            out[(Nx - 2) * Ny + row] = -in[(Nx - 5) * Ny + row] / 60 + 3 * in[(Nx - 4) * Ny + row] / 20 - 3 * in[(Nx - 3) * Ny + row] / 4 +
-                                       3 * in[(Nx - 1) * Ny + row] / 4 - 3 * in[row] / 20 + in[Ny + row] / 60;
-
-            out[(Nx - 1) * Ny + row] = -in[(Nx - 4) * Ny + row] / 60 + 3 * in[(Nx - 3) * Ny + row] / 20 - 3 * in[(Nx - 2) * Ny + row] / 4 +
-                                       3 * in[row] / 4 - 3 * in[Ny + row] / 20 + in[2 * Ny + row] / 60;
-
-            for (int col = 3; col < Nx - 3; ++col)
+            for (int col = 0; col < Nx; ++col)
             {
-                out[row + Ny * col] = -in[row + Ny * (col - 3)] / 60 + 3 * in[row + Ny * (col - 2)] / 20 - 3 * in[row + Ny * (col - 1)] / 4 +
-                                      3 * in[row + Ny * (col + 1)] / 4 - 3 * in[row + Ny * (col + 2)] / 20 + in[row + Ny * (col + 3)] / 60;
+                out[Ny * col] = -in[Ny * (col + 1) - 3] / 60 + 3 * in[Ny * (col + 1) - 2] / 20 - 3 * in[Ny * (col + 1) - 1] / 4 +
+                                3 * in[Ny * col + 1] / 4 - 3 * in[Ny * col + 2] / 20 + in[Ny * col + 3] / 60;
+
+                out[Ny * col + 1] = -in[Ny * (col + 1) - 2] / 60 + 3 * in[Ny * (col + 1) - 1] / 20 - 3 * in[Ny * col] / 4 +
+                                    3 * in[Ny * col + 2] / 4 - 3 * in[Ny * col + 3] / 20 + in[Ny * col + 4] / 60;
+
+                out[Ny * col + 2] = -in[Ny * (col + 1) - 1] / 60 + 3 * in[Ny * col] / 20 - 3 * in[Ny * col + 1] / 4 +
+                                    3 * in[Ny * col + 3] / 4 - 3 * in[Ny * col + 4] / 20 + in[Ny * col + 5] / 60;
+
+                out[Ny * (col + 1) - 3] = -in[Ny * (col + 1) - 6] / 60 + 3 * in[Ny * (col + 1) - 5] / 20 - 3 * in[Ny * (col + 1) - 4] / 4 +
+                                          3 * in[Ny * (col + 1) - 2] / 4 - 3 * in[Ny * (col + 1) - 1] / 20 + in[Ny * col] / 60;
+
+                out[Ny * (col + 1) - 2] = -in[Ny * (col + 1) - 5] / 60 + 3 * in[Ny * (col + 1) - 4] / 20 - 3 * in[Ny * (col + 1) - 3] / 4 +
+                                          3 * in[Ny * (col + 1) - 1] / 4 - 3 * in[Ny * col] / 20 + in[Ny * col + 1] / 60;
+
+                out[Ny * (col + 1) - 1] = -in[Ny * (col + 1) - 4] / 60 + 3 * in[Ny * (col + 1) - 3] / 20 - 3 * in[Ny * (col + 1) - 2] / 4 +
+                                          3 * in[Ny * col] / 4 - 3 * in[Ny * col + 1] / 20 + in[Ny * col + 2] / 60;
+
+                for (int row = 3; row < Ny - 3; ++row)
+                {
+                    out[row + Ny * col] = -in[row - 3 + Ny * col] / 60 + 3 * in[row - 2 + Ny * col] / 20 - 3 * in[row - 1 + Ny * col] / 4 +
+                                          3 * in[row + 1 + Ny * col] / 4 - 3 * in[row + 2 + Ny * col] / 20 + in[row + 3 + Ny * col] / 60;
+                }
             }
         }
     }
-    void LoopDerivativeY(double *in, double *out)
-    {
-        for (int col = 0; col < Nx; ++col)
-        {
-            out[Ny * col] = -in[Ny * (col + 1) - 3] / 60 + 3 * in[Ny * (col + 1) - 2] / 20 - 3 * in[Ny * (col + 1) - 1] / 4 +
-                            3 * in[Ny * col + 1] / 4 - 3 * in[Ny * col + 2] / 20 + in[Ny * col + 3] / 60;
-
-            out[Ny * col + 1] = -in[Ny * (col + 1) - 2] / 60 + 3 * in[Ny * (col + 1) - 1] / 20 - 3 * in[Ny * col] / 4 +
-                                3 * in[Ny * col + 2] / 4 - 3 * in[Ny * col + 3] / 20 + in[Ny * col + 4] / 60;
-
-            out[Ny * col + 2] = -in[Ny * (col + 1) - 1] / 60 + 3 * in[Ny * col] / 20 - 3 * in[Ny * col + 1] / 4 +
-                                3 * in[Ny * col + 3] / 4 - 3 * in[Ny * col + 4] / 20 + in[Ny * col + 5] / 60;
-
-            out[Ny * (col + 1) - 3] = -in[Ny * (col + 1) - 6] / 60 + 3 * in[Ny * (col + 1) - 5] / 20 - 3 * in[Ny * (col + 1) - 4] / 4 +
-                                      3 * in[Ny * (col + 1) - 2] / 4 - 3 * in[Ny * (col + 1) - 1] / 20 + in[Ny * col] / 60;
-
-            out[Ny * (col + 1) - 2] = -in[Ny * (col + 1) - 5] / 60 + 3 * in[Ny * (col + 1) - 4] / 20 - 3 * in[Ny * (col + 1) - 3] / 4 +
-                                      3 * in[Ny * (col + 1) - 1] / 4 - 3 * in[Ny * col] / 20 + in[Ny * col + 1] / 60;
-
-            out[Ny * (col + 1) - 1] = -in[Ny * (col + 1) - 4] / 60 + 3 * in[Ny * (col + 1) - 3] / 20 - 3 * in[Ny * (col + 1) - 2] / 4 +
-                                      3 * in[Ny * col] / 4 - 3 * in[Ny * col + 1] / 20 + in[Ny * col + 2] / 60;
-
-            for (int row = 3; row < Ny - 3; ++row)
-            {
-                out[row + Ny * col] = -in[row - 3 + Ny * col] / 60 + 3 * in[row - 2 + Ny * col] / 20 - 3 * in[row - 1 + Ny * col] / 4 +
-                                      3 * in[row + 1 + Ny * col] / 4 - 3 * in[row + 2 + Ny * col] / 20 + in[row + 3 + Ny * col] / 60;
-            }
-        }
-    }
-    void TimeIntegrate(char type)
+    void TimeIntegrate()
     {
         // Initialising variables
         double *u = new double[Nx * Ny];
@@ -211,14 +260,14 @@ public:
             }
 
             // Calculating K1
-            xDerivative(type, u, dudx);
-            yDerivative(type, u, dudy);
-            xDerivative(type, v, dvdx);
-            yDerivative(type, v, dvdy);
-            xDerivative(type, h, dhdx);
-            yDerivative(type, h, dhdy);
-            xDerivative(type, hu, dhudx);
-            yDerivative(type, hv, dhvdy);
+            xDerivative(u, dudx);
+            yDerivative(u, dudy);
+            xDerivative(v, dvdx);
+            yDerivative(v, dvdy);
+            xDerivative(h, dhdx);
+            yDerivative(h, dhdy);
+            xDerivative(hu, dhudx);
+            yDerivative(hv, dhvdy);
 
             for (int i = 0; i < Nx * Ny; ++i)
             {
@@ -237,14 +286,14 @@ public:
                 hv[i] = h[i] * v[i];
             }
 
-            xDerivative(type, u, dudx);
-            yDerivative(type, u, dudy);
-            xDerivative(type, v, dvdx);
-            yDerivative(type, v, dvdy);
-            xDerivative(type, h, dhdx);
-            yDerivative(type, h, dhdy);
-            xDerivative(type, hu, dhudx);
-            yDerivative(type, hv, dhvdy);
+            xDerivative(u, dudx);
+            yDerivative(u, dudy);
+            xDerivative(v, dvdx);
+            yDerivative(v, dvdy);
+            xDerivative(h, dhdx);
+            yDerivative(h, dhdy);
+            xDerivative(hu, dhudx);
+            yDerivative(hv, dhvdy);
 
             for (int i = 0; i < Nx * Ny; ++i)
             {
@@ -263,14 +312,14 @@ public:
                 hv[i] = h[i] * v[i];
             }
 
-            xDerivative(type, u, dudx);
-            yDerivative(type, u, dudy);
-            xDerivative(type, v, dvdx);
-            yDerivative(type, v, dvdy);
-            xDerivative(type, h, dhdx);
-            yDerivative(type, h, dhdy);
-            xDerivative(type, hu, dhudx);
-            yDerivative(type, hv, dhvdy);
+            xDerivative(u, dudx);
+            yDerivative(u, dudy);
+            xDerivative(v, dvdx);
+            yDerivative(v, dvdy);
+            xDerivative(h, dhdx);
+            yDerivative(h, dhdy);
+            xDerivative(hu, dhudx);
+            yDerivative(hv, dhvdy);
 
             for (int i = 0; i < Nx * Ny; ++i)
             {
@@ -289,14 +338,14 @@ public:
                 hv[i] = h[i] * v[i];
             }
 
-            xDerivative(type, u, dudx);
-            yDerivative(type, u, dudy);
-            xDerivative(type, v, dvdx);
-            yDerivative(type, v, dvdy);
-            xDerivative(type, h, dhdx);
-            yDerivative(type, h, dhdy);
-            xDerivative(type, hu, dhudx);
-            yDerivative(type, hv, dhvdy);
+            xDerivative(u, dudx);
+            yDerivative(u, dudy);
+            xDerivative(v, dvdx);
+            yDerivative(v, dvdy);
+            xDerivative(h, dhdx);
+            yDerivative(h, dhdy);
+            xDerivative(hu, dhudx);
+            yDerivative(hv, dhvdy);
 
             for (int i = 0; i < Nx * Ny; ++i)
             {
@@ -336,9 +385,13 @@ private:
     int Nx;
     int Ny;
     int ic;
+    char type;
     double *U;
     double *V;
     double *H;
+    double *stencil;
+    double *extrarowstencil;
+    double *extracolumnstencil;
 };
 
 int main(int argc, char **argv)
@@ -349,7 +402,7 @@ int main(int argc, char **argv)
 
     wave.SetInitialConditions();
 
-    wave.TimeIntegrate('L');
+    wave.TimeIntegrate();
 
     wave.output();
 }
--- makefile
+++ makefile
@@ -1,4 +1,12 @@
 default: main.out
 
+.PHONY: runblas
+runblas: main.out
+	time ./main.out --dt 0.1 --T 80 --Nx 100 --Ny 100 --ic 3 --type B
+
+.PHONY: runloop
+runloop: main.out
+	time ./main.out --dt 0.1 --T 80 --Nx 100 --Ny 100 --ic 3 --type L
+	
 main.out: main.cpp
-	g++ -O3 -o main.out main.cpp -lboost_program_options
+	g++ -O3 -o main.out main.cpp -lblas -lboost_program_options
